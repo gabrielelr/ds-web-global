6b0175b84302ddef1aafe757683eeef5501849e2
gabrielelr <gabriele.larosa.ext@gmail.it>
2026-05-22 17:59:28 +0200

    add Storybook (svelte-vite) + Tailwind preview wiring + lib/Button story

---
+++ src/lib/Button.stories.svelte
@@ -0,0 +1,24 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Button from './Button.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Lib/Button',
+    component: Button,
+    tags: ['autodocs'],
+    argTypes: {
+      variant: {
+        control: { type: 'select' },
+        options: ['primary', 'secondary', 'ghost'],
+      },
+    },
+  });
+</script>
+
+{#snippet template(args)}
+  <Button {...args}>Click me</Button>
+{/snippet}
+
+<Story name="Primary" args={{ variant: 'primary' }} children={template} />
+<Story name="Secondary" args={{ variant: 'secondary' }} children={template} />
+<Story name="Ghost" args={{ variant: 'ghost' }} children={template} />
